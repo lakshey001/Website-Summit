edbfccf8e39b0be2365766ff5f7d4dbb2c52c3bc
kunalvishnoi <vishnoi.kunal0@gmail.com>
2017-12-07 20:55:44 +0530

    update image

--- js/script.js
+++ js/script.js
@@ -51,7 +51,24 @@ $(document).ready(function(){
     });
 });
 
-
+function animationHover(element, animation){
+    element = $(element);
+    element.hover(
+        function() {
+            element.addClass('animated ' + animation);        
+        },
+        function(){
+            //wait for animation to finish before removing classes
+            window.setTimeout( function(){
+                element.removeClass('animated ' + animation);
+            }, 2000);         
+        });
+}
+$(document).ready(function(){
+    $('.animate').each(function() {
+        animationHover(this, 'slideInLeft');
+    });
+});
 
   
  
